f658dcec22d33976b85f49f8cf311670aed56ea4
martudiaz <martinadiaz780@gmail.com>
2025-11-11 00:12:33 -0300

    slider js

--- js/script.js
+++ js/script.js
@@ -2,27 +2,30 @@ import { Ingreso } from "./ingreso.js";
 import { Cursos } from "./cursos.js";
 import { Calendario } from "./calendario.js";
 import { Home } from "./home.js";
+import { Slider } from "./slider.js";
 
 export let ingreso;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // 1. Inicializacion de los cursos
+
+    
+    const sliderContainer = document.querySelector('.slider-container');
+    if (sliderContainer) {
+        new Slider(sliderContainer, 5000); 
+    }
+
     const cursos = new Cursos();
     cursos.inicializarDetalle();
     cursos.mostrarRecomendados('#recomendadosContenedor', '/pages/detalleCurso.html');
 
-    // 2. Funcionamiento de la sesion
     ingreso = new Ingreso();
     if (document.body.classList.contains('profile-page') || document.getElementById('perfilNombre')) {
         ingreso.populateProfile();
     }
 
-    // 3. Carga de cursos en el calendario
     const calendario = new Calendario();
-    
 
-    // 4. Carga de cursos en el Home
     const cursosHome = new Home('#cursosContenedor', '/pages/detalleCurso.html');
     cursosHome.mostrarCursosHome();
 
---
+++ js/slider.js
@@ -0,0 +1,52 @@
+
+export class Slider {
+  constructor(sliderContainer, autoPlayInterval = 5000) {
+    this.container = sliderContainer;
+    this.autoPlayInterval = autoPlayInterval;
+    this.slides = this.container.querySelectorAll('.slide');
+    this.dots = this.container.querySelectorAll('.dot');
+    this.currentSlide = 0;
+    this.autoPlayIntervalId = null;
+    this.init();
+  }
+
+  init() {
+    this.dots.forEach((dot, index) => {
+      dot.addEventListener('click', () => {
+        this.stopAutoPlay();
+        this.showSlide(index);
+        this.startAutoPlay();
+      });
+    });
+    this.startAutoPlay();
+  }
+
+  showSlide(index) {
+    this.currentSlide = index;
+    this.slides.forEach(slide => {
+      slide.classList.remove('active');
+    });
+    this.dots.forEach(dot => {
+      dot.classList.remove('active');
+    });
+    this.slides[this.currentSlide].classList.add('active');
+    this.dots[this.currentSlide].classList.add('active');
+  }
+
+  nextSlide() {
+    const nextIndex = (this.currentSlide + 1) % this.slides.length;
+    this.showSlide(nextIndex);
+  }
+
+  startAutoPlay() {
+    this.autoPlayIntervalId = setInterval(() => {
+      this.nextSlide();
+    }, this.autoPlayInterval);
+  }
+
+  stopAutoPlay() {
+    if (this.autoPlayIntervalId) {
+      clearInterval(this.autoPlayIntervalId);
+    }
+  }
+}

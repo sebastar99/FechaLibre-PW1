0d5527cb08bf75abd141075ce8eaf2a2c2ad7d72
sebastar99 <datahola22@gmail.com>
2025-11-11 23:07:34 -0300

    union

---
+++ js/contacto.js
@@ -0,0 +1,85 @@
+export class Contacto{
+
+    constructor(){
+        const form= document.querySelector ("#form");
+        const consulta= document.querySelector("#consulta");
+        const contador = document.querySelector("#contadorConsulta");
+        const dialogo = document.querySelector("#dialogo");
+        const mensaje = document.querySelector("#dialogoMensaje");
+        const btnCerrar = document.querySelector("#dialogoCerrar");
+
+        
+       consulta.addEventListener("input", () => {
+         const total = consulta.value.length;
+         contador.textContent = `${total} / 1000 caracteres`;
+       });
+
+        
+         btnCerrar.addEventListener("click", () => {
+            dialogo.close()
+         window.location.href = "/home.html"
+        });
+
+
+        form.addEventListener("submit", (event)=>{
+
+            event.preventDefault();
+            const nombre=document.querySelector ("#username");
+            const apellido=document.querySelector ("#surname");
+            const email=document.querySelector ("#email");
+            const numeroTel=document.querySelector ("#phone");
+            
+           function mostrarDialogo(texto) {
+             mensaje.textContent = texto;
+             dialogo.showModal();
+            }
+
+           function validaCampos (){
+                const nomAValidar= nombre.value.trim();
+                const apeAValidar= apellido.value.trim();
+                if(!nomAValidar || !apeAValidar){
+                 mostrarDialogo ("Nombre y apellido no pueden estar vacios")
+                 return false;
+
+                 }
+                const emailAValidar= email.value.trim();
+                const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+                if (!emailValido.test(emailAValidar)) {
+                 mostrarDialogo("Por favor ingresá un email válido");
+                 return false;
+                } 
+
+                const telAValidar= numeroTel.value.trim();
+                const telValido= /^(\d{4}-\d{4})/
+
+                if(telAValidar !== ""){
+                    if(!telValido.test(telAValidar)){
+                        mostrarDialogo("El teléfono debe tener 8 dígitos seprados por un guión en el medio (ej: 1234-5678)");
+                        return false;
+                    }
+                }
+                const texto = consulta.value.trim();
+                if(texto.length>1000){
+                    return false;
+                }
+
+
+             return true;
+
+            }
+
+        
+         if (validaCampos()){
+             mostrarDialogo("Consulta enviada");
+
+            }
+            
+        })
+
+    }
+    
+
+    
+
+}
--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ import { Ingreso } from "./ingreso.js";
 import { Cursos } from "./cursos.js";
 import { Calendario } from "./calendario.js";
 import { Home } from "./home.js";
+import { Giftcard } from "./giftcard.js";
+import { Contacto } from "./contacto.js";
+import { Slider } from "./slider.js";
+import { InscripcionEmpresa } from "./inscripcionEmpresa.js";
 
 export let ingreso;
 
@@ -19,11 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // 3. Carga de cursos en el calendario
-    const calendario = new Calendario();
-    
+    new Calendario();
 
     // 4. Carga de cursos en el Home
     const cursosHome = new Home('#cursosContenedor', '/pages/detalleCurso.html');
     cursosHome.mostrarCursosHome();
 
+    // 5. Lógica para contacto
+    if (document.querySelector('#form')) {
+        new Contacto();
+    }
+
+    // 6. Logica para carrousel
+    const sliderContainer = document.querySelector('.slider-container');
+    if (sliderContainer) {
+        new Slider(sliderContainer, 5000);
+    }
+
+    // 8. Logica giftcard
+    new Giftcard();
+
+    // 9. Logica inscripcion para empresas
+    new InscripcionEmpresa();
 });
+
+
